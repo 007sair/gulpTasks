5b019b5079544a51ad4a0e35b21bc2b13200decf
longchan <longchan@mia.com>
2017-09-12 16:56:10 +0800

    update

--- gulp/tasks_pub/pub-sass.js
+++ gulp/tasks_pub/pub-sass.js
@@ -17,18 +17,32 @@ module.exports = function (gulp, config, $, args) {
         }
 
         gulp.task('sass:' + projectName, function () {
-            return gulp.src(obj.src)
-                .pipe($.sourcemaps.init())
-                .pipe($.sass({
-                    includePaths: ['./resources/'], //让sass中import的路径包含定义的字段
-                    precision: 4 //保留小数点后几位 #https://github.com/sass/node-sass#precision
-                })
-                .on('error', $.sass.logError))
-                .pipe($.postcss(config.css.postCss))
-                .pipe($.cleanCss(config.css.cleanCss))
-                .pipe($.sourcemaps.write('.'))
-                .pipe(gulp.dest(obj.dest))
-                .pipe($.connect.reload())
+            if (args.debug) {
+                return gulp.src(obj.src)
+                    .pipe($.sourcemaps.init())
+                    .pipe($.sass({
+                        includePaths: ['./resources/'], //让sass中import的路径包含定义的字段
+                        precision: 4 //保留小数点后几位 #https://github.com/sass/node-sass#precision
+                    })
+                    .on('error', $.sass.logError))
+                    .pipe($.postcss(config.css.postCss))
+                    .pipe($.cleanCss(config.css.cleanCss))
+                    .pipe($.sourcemaps.write('.'))
+                    .pipe(gulp.dest(obj.dest))
+                    .pipe($.connect.reload())
+            } else {
+                return gulp.src(obj.src)
+                    .pipe($.sass({
+                        includePaths: ['./resources/'], //让sass中import的路径包含定义的字段
+                        precision: 4 //保留小数点后几位 #https://github.com/sass/node-sass#precision
+                    })
+                    .on('error', $.sass.logError))
+                    .pipe($.postcss(config.css.postCss))
+                    .pipe($.cleanCss(config.css.cleanCss))
+                    .pipe(gulp.dest(obj.dest))
+                    .pipe($.connect.reload())
+            }
+
         });
 
     }
